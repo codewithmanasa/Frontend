0005ac89880aade896b78aafe552a13285d99e65
priyssri923 <chitra.codehub.java@gmail.com>
2026-02-22 13:57:29 +0530

    API_URL update

--- src/services/Api.jsx
+++ src/services/Api.jsx
@@ -3,7 +3,7 @@ import axios from "axios";
 const API_URL = import.meta.env.VITE_API_URL || "http://localhost:8080/api";
 
 const api = axios.create({
-  baseURL: `S{API_URL}/api`,
+  baseURL: API_URL,
   headers: {
     "Content-Type": "application/json",
   },
